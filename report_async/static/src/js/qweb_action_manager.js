1f3a04b323a50e6795f2fd980fc41e1d98e8c28d
KKamaa <kevkamaa96@gmail.com>
2022-03-14 17:24:07 +0300

    [FIX] changes

--- report_async/static/src/js/qweb_action_manager.js
+++ report_async/static/src/js/qweb_action_manager.js
@@ -17,57 +17,69 @@ odoo.define('report_async.print', function(require) {
             var _action = _.clone(action);
             var $content = $(QWeb.render("ReportAsyncConfiguration", {}));
 
-            // Popup for async Configuration
-            new Dialog(self, {
-                title: _t("Async Report Configuration ") + '(' + action['display_name'] + ')',
-                size : "medium",
-                buttons: [{
-                    text: _t("Print"),
-                    classes: 'btn-primary',
-                    close: true,
-                    click: function () {
-                        var is_report_async = this.$('#async_report_checker')
-                            .prop('checked');
-                        var user_email = this.$('#async-user-email').val();
-                        if (user_email !== '' && is_report_async) {
-                            // Try basic email validation
-                            if (self._validate_email(user_email)) {
-                                if ('report_type' in _action && _action.report_type === 'qweb-pdf') {
-                                    framework.unblockUI();
-                                    // Generate report async
-                                    new Model('report.async').call('print_document_async',
-                                        [_action.context.active_ids, _action.report_name],
-                                        {
-                                            to_email: user_email,
-                                            data: _action.data || {},
-                                            context: _action.context || {},
-                                        }).then(function() {
-                                            self.do_notify(_t('Report'),
-                                                _t('Job started to generate report. Upon ' +
-                                                    'completion, mail sent to:') +
-                                                user_email, true);
-                                        }).fail(function() {
-                                            self.do_notify(_t('Report'),
-                                                _t('Failed, error on job creation.'), true);
-                                        });
-                                }
-                                else {
-                                    // default to normal approach to generate report
-                                    return _super.apply(self, [action, options]);
-                                }
-                            }
-                        }
-                        else {
-                            // default to normal approach to generate report
-                            return _super.apply(self, [action, options]);
-                        }
-                    }},
-                    {
-                        text: _t("Discard"),
-                        close: true
-                    }],
-                $content: $content,
-            }).open();
+            new Model('ir.actions.report.xml')
+                .call('is_report_async', [_action.report_name])
+                .then(function(result){
+                    if (result.is_report_async) {
+                        // Popup for async Configuration
+                        var asyncDialog = new Dialog(self, {
+                            title: _t("Async Report Configuration ") + '(' + action['display_name'] + ')',
+                            size : "medium",
+                            buttons: [{
+                                text: _t("Print"),
+                                classes: 'btn-primary',
+                                close: true,
+                                click: function () {
+                                    var is_report_async = this.$('#async_report_checker')
+                                        .prop('checked');
+                                    var user_email = this.$('#async-user-email').val();
+                                    if (user_email !== '' && is_report_async) {
+                                        // Try basic email validation
+                                        if (self._validate_email(user_email)) {
+                                            if ('report_type' in _action && _action.report_type === 'qweb-pdf') {
+                                                framework.unblockUI();
+                                                // Generate report async
+                                                new Model('report.async').call('print_document_async',
+                                                    [_action.context.active_ids, _action.report_name],
+                                                    {
+                                                        to_email: user_email,
+                                                        data: _action.data || {},
+                                                        context: _action.context || {},
+                                                    }).then(function() {
+                                                        self.do_notify(_t('Report'),
+                                                            _t('Job started to generate report. Upon ' +
+                                                                'completion, mail sent to:') +
+                                                            user_email, true);
+                                                    }).fail(function() {
+                                                        self.do_notify(_t('Report'),
+                                                            _t('Failed, error on job creation.'), true);
+                                                    });
+                                            }
+                                            else {
+                                                // default to normal approach to generate report
+                                                return _super.apply(self, [action, options]);
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        // default to normal approach to generate report
+                                        return _super.apply(self, [action, options]);
+                                    }
+                                }},
+                                {
+                                    text: _t("Discard"),
+                                    close: true
+                                }],
+                            $content: $content,
+                        }).open();
+                        asyncDialog.$el.find("#async-user-email").val(
+                            result.mail_recipient);
+                    }
+                    else {
+                        // default to normal approach to generate report
+                        return _super.apply(self, [action, options]);
+                    }
+                });
             },
         _validate_email: function (email) {
             var res = email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
